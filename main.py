f8c33575f260f768dbb3040b012e6b8178e825b9
javiervalle-cmyk <javier_valle@siman.com>
2026-09-07 11:21:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print("Hola Javier")
-print("Python esta funcionando en GitHub Actions")
+import requests
+
+url = "https://sv.siman.com/api/catalog_system/pub/products/search?_from=0&_to=49"
+
+respuesta = requests.get(
+    url,
+    headers={
+        "Accept": "application/json"
+    },
+    timeout=60
+)
+
+print("Status:", respuesta.status_code)
+
+productos = respuesta.json()
+
+print("Productos recibidos:", len(productos))
+
+if len(productos) > 0:
+    print("Primer producto:")
+    print(productos[0]["productName"])
